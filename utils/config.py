d327047df96de8b99f32a4098e1be7b0ba18db3b
sam-dee <mishat.tarasov2001@gmail.com>
2022-10-09 21:15:41 +0300

    Added configs * config parser

---
+++ utils/config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Config class"""
+
+import json
+
+
+class Config:
+    """Config class which contains data, train and model hyperparameters"""
+
+    def __init__(self, data, train, model):
+        self.data = data
+        self.train = train
+        self.model = model
+
+    @classmethod
+    def from_json(cls, cfg):
+        """Creates config from json"""
+        params = json.loads(json.dumps(cfg), object_hook=HelperObject)
+        return cls(params.data, params.train, params.model)
+
+    @classmethod
+    def from_file(cls, path):
+        """Creates config from json file"""
+        params = json.load(open(path, 'r'), object_hook=HelperObject)
+        return cls(params.data, params.train, params.model)
+
+
+class HelperObject(object):
+    """Helper class to convert json into Python object"""
+
+    def __init__(self, dict_):
+        self.__dict__.update(dict_)
